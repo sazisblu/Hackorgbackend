ba1563438b239defe8d7444b55fafb9b62cf2c6b
maneedasshrestha <shresthamaneedas852@gmail.com>
2026-03-11 22:04:17 +0545

    fix(): add x-admin-id to allowedHeaders in CORS configuration

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(
     origin: process.env.FRONTEND_URL, // Adjust to your frontend URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-admin-id"],
   }),
 );
 
